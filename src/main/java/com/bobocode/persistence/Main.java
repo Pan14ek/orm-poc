4332f59704b08d15ea36a3bd2821b1875fc86623
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-01-20 19:47:57 +0200

    Update the demo class - Main

--- src/main/java/com/bobocode/persistence/Main.java
+++ src/main/java/com/bobocode/persistence/Main.java
@@ -3,15 +3,21 @@ package com.bobocode.persistence;
 import com.bobocode.persistence.api.EntityManager;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SimpleORM simpleORM = new SimpleORM();
 
-        EntityManager entityManager = simpleORM.entityManager();
+        try (EntityManager entityManager = simpleORM.entityManager()) {
+            Person person = entityManager.find(Person.class, 1);
+            System.out.println(person);
 
-        Person person = entityManager.find(Person.class, 1);
-        System.out.println(person);
+            person.setFirstName("Name was updated");
+            person.setLastName("Surname was updated");
+        }
 
-        Person person1 = entityManager.find(Person.class, 1);
-        System.out.println(person1);
+        try (EntityManager entityManager = simpleORM.entityManager()) {
+            Person person1 = entityManager.find(Person.class, 1);
+            System.out.println(person1);
+        }
+
     }
 }
